ed212f0ca8802b8995a678142a995dcf5e491eae
Armen Arakelov <a.g.arakelov@gmail.com>
2025-02-18 20:34:36 +0100

    Simplify post-deploy command in PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '.env.deploy' });
 
 module.exports = {
   apps: [
@@ -8,10 +8,18 @@ module.exports = {
       instances: 'max',
       exec_mode: 'cluster',
       env: {
-        NODE_ENV: process.env.NODE_ENV,
+        NODE_ENV: 'production',
         PORT: process.env.PORT,
         DB_URL: process.env.DB_URL,
         JWT_SECRET: process.env.JWT_SECRET,
+      },
+    },
+    {
+      name: 'mesto-frontend',
+      script: 'npx serve -s build',
+      env: {
+        NODE_ENV: 'production',
+        PORT: process.env.FRONTEND_PORT,
       },
     },
   ],
@@ -23,9 +31,17 @@ module.exports = {
       ref: 'origin/main',
       repo: process.env.GIT_REPO,
       path: process.env.SERVER_PATH,
+
+      'pre-setup': 'sudo apt update && sudo apt install -y git',
       'post-setup': 'ls -la',
-      'pre-deploy': `scp ./*.env ${process.env.SERVER_USER}@${process.env.SERVER_HOST}:${process.env.SERVER_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+
+      'post-deploy': `
+        export NVM_DIR=~/.nvm
+        source ~/.nvm/nvm.sh
+        npm install
+        npm run build
+        pm2 reload ecosystem.config.js --env production
+      `,
     },
   },
 };
